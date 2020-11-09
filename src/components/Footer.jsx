5b4adcbd8107f1da7ed5af654b232ecf19ae6463
zao95 <zao95@naver.com>
2020-11-09 20:18:18 +0900

    Update Footer and Nav components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/common/Footer.sass"
+
+const Footer = () => {
+	return (
+		<footer></footer>
+	)
+}
+
+export default Footer
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/common/Nav.sass"
+
+const Nav = () => {
+	return (
+        <nav>
+            
+        </nav>
+	)
+}
+
+export default Nav
